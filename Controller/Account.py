bc3b4f18aadc8cc6a9b0d12287c0f406dcee8311
Ronpu <ronpu@hotmail.fr>
2024-01-18 19:09:38 +0100

    PUT account avec token + "me"  ATTENTION : une erreur arrive parfois sans que je comprenne pk

--- Controller/Account.py
+++ Controller/Account.py
@@ -8,14 +8,16 @@ from Util.API import returnAPIFormat
 route_blueprint = Blueprint('account', __name__)
 
 
-### NORMALEMENT uniquement ton compte via l'uid ou l'alias "me". Sauf si tu as un compte avec le role ROLE_ADMIN ou tu peux voir tous les comptes.
 # GET account
 @route_blueprint.route("/account", methods=["GET"])
 def getAccount():
 	# try: # A ENLEVER LORS DU DEBUG
 		param = request.get_json()
-		uid = param["uid"]
-		userToken = param["userToken"]
+		try:
+			userToken = param["userToken"]
+			uid = param["uid"]
+		except:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
 		if uid is None or userToken is None:
 			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
 		
@@ -25,7 +27,7 @@ def getAccount():
 		"""
 		dataUser = Database.request(sql, (userToken,))
 		if dataUser is None:
-			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 1")
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 		if not dataUser:
 			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Token error")
 
@@ -36,7 +38,7 @@ def getAccount():
 		sql = "SELECT name FROM role r, account_role ar WHERE ar.account_id = ? AND r.id = ar.role_id"
 		data = Database.request(sql, (dataUser[0][0],))
 		if data is None:
-			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 2")
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 		
 		roleAdmin = False
 		for i in data:
@@ -52,7 +54,7 @@ def getAccount():
 				sql = "SELECT uid, username, createdAt, updatedAt FROM account WHERE uid = ?"
 				dataAccount = Database.request(sql, (uid,))
 				if dataAccount is None:
-					return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 3")
+					return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 				if not dataAccount:
 					return returnAPIFormat(data=None, link=request.path, method=request.method, status=404)
 		
@@ -86,7 +88,7 @@ def getAccount():
 			"""
 			dataRole = Database.request(sql, (bonUid,))
 			if dataRole is None:
-				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 4")
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 				
 			for i in dataRole:
 				data["role"].append(i[1])
@@ -101,7 +103,7 @@ def getAccount():
 				sql = "SELECT uid, username, createdAt, updatedAt FROM account WHERE uid = ?"
 				dataAccount = Database.request(sql, (uid,))
 				if dataAccount is None:
-					return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 5")
+					return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 
 				data = {
 					"uid": dataAccount[0][0],
@@ -129,7 +131,7 @@ def getAccount():
 			"""
 			dataRole = Database.request(sql, (bonUid,))
 			if dataRole is None:
-				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 6")
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 				
 			for i in dataRole:
 				data["role"].append(i[1])
@@ -140,14 +142,19 @@ def getAccount():
 	# 	return returnAPIFormat(data=None, link=request.path, method=request.method, status=404)
 
 
+
+
 # POST account
 @route_blueprint.route("/account", methods=["POST"])
 def postAccount():
 	param = request.get_json()
-	username = param["username"]
-	password = param["password"]
-	roles = param["roles"]
-	status = param["status"]
+	try:
+		username = param["username"]
+		password = param["password"]
+		roles = param["roles"]
+		status = param["status"]
+	except:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
 	uid = str(uuid.uuid4())
 
 	if username is None or password is None or roles is None or status is None:
@@ -161,7 +168,6 @@ def postAccount():
 	if data:
 		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, le l'username existe déjà")
 
-	
 	# RECUP les uid des roles + CHECK s'ils existent
 	listeRoleId = []
 	listeRoleName = []
@@ -169,6 +175,8 @@ def postAccount():
 		sql = "SELECT id, name FROM role WHERE name = ?"
 		data = Database.request(sql, (i,))
 		if data is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		if not data:
 			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, le role '"+i+"' n'existe pas")
 		listeRoleId.append(data[0][0])
 		listeRoleName.append(data[0][1])
@@ -206,69 +214,170 @@ def postAccount():
 	return returnAPIFormat(data=data, link=request.path, method=request.method)
 
 
-### NORMALEMENT uniquement ton compte via l'uid ou l'alias "me". Sauf si tu as un compte avec le role ROLE_ADMIN ou tu peux modifier tous les comptes.
+
+
 # PUT account
 @route_blueprint.route("/account", methods=["PUT"])
 def putAccount():
 	param = request.get_json()
-	uid = param["uid"]
-	username = param["username"]
-	password = param["password"]
-	roles = param["roles"]
-	status = param["status"]
-	if uid is None or username is None or password is None or roles is None or status is None:
+	try:
+		userToken = param["userToken"]
+		uid = param["uid"]
+		username = param["username"]
+		password = param["password"]
+		status = param["status"]
+	except:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
+	if uid is None or username is None or password is None or status is None:
 		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
 	if status != 'open' and status != 'closed':
 		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, status peut uniquement être 'open' ou 'closed'")
 	
-	# CHECK si l'account existe
-	sql = "SELECT id FROM account WHERE uid = ?"
-	dataAccountId = Database.request(sql, (uid,))
-	if not dataAccountId:
-		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, l'account n'existe pas")
-	
-	# RECUP les uid des roles + CHECK s'ils existent
-	listeRoleId = []
-	listeRoleName = []
-	for i in roles:
-		sql = "SELECT id, name FROM role WHERE name = ?"
-		data = Database.request(sql, (i,))
+	# SELECT l'user par le token + verif si token est toujours valide
+	sql = """SELECT id, uid, username, createdAt, updatedAt, CASE WHEN dateExpire_Token > current_timestamp THEN 1
+	WHEN dateExpire_Token <= current_timestamp THEN 0 END FROM account WHERE token = ?
+	"""
+	dataUser = Database.request(sql, (userToken,))
+	if dataUser is None:
+		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+	if not dataUser:
+		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Token error")
+
+	if dataUser[0][5] == 0:
+		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Token plus valide")
+
+	# SELECT les roles de l'user
+	sql = "SELECT name FROM role r, account_role ar WHERE ar.account_id = ? AND r.id = ar.role_id"
+	data = Database.request(sql, (dataUser[0][0],))
+	if data is None:
+		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		
+	roleAdmin = False
+	for i in data:
+		if (i[0] == 'ROLE_ADMIN'):
+			roleAdmin = True
+			break
+
+	if roleAdmin == False:	# SI USER N'EST PAS ADMIN
+			
+		if uid != "me":
+			# CHECK si l'account existe
+			sql = "SELECT id FROM account WHERE uid = ?"
+			dataAccountId = Database.request(sql, (uid,))
+			if dataAccountId is None:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+			if not dataAccountId:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=404)
+		
+			# VERIF si c'est bien le même compte demandé que celui utilisé
+			if dataAccountId[0][0] != dataUser[0][0]:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=404) # 404 car on ne veut pas montrer qu'il y a bien un account avec cet uid
+			
+			bonId = dataAccountId[0][0]
+
+		else:	# SI uid = "me", pas besoin de select
+			bonId = dataUser[0][0]
+
+		# UPDATE de l'account
+		sql = "UPDATE account SET username = ?, password = ?, status = ?, updatedAt = current_timestamp WHERE id = ?"
+		data = Database.request(sql, (username, password, status, bonId))
 		if data is None:
-			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, le role '"+i+"' n'existe pas")
-		listeRoleId.append(data[0][0])
-		listeRoleName.append(data[0][1])
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
 
-	# UPDATE de l'account
-	sql = "UPDATE account SET username = ?, password = ?, status = ?, updatedAt = current_timestamp WHERE id = ?"
-	data = Database.request(sql, (username, password, status, dataAccountId[0][0]))
-	if data is None:
-		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 1")
+		# SELECT les infos de l'account après les modifications
+		sql = "SELECT uid, username, password, createdAt, updatedAt FROM account WHERE id = ?"
+		data = Database.request(sql, (bonId,))
+		if data is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		
+		# SELECT les roles de l'account
+		listeRoleName = []
+		sql = "SELECT name FROM role r, account_role ar WHERE ar.account_id = ? AND r.id = ar.role_id"
+		dataRole = Database.request(sql, (bonId,))
+		if dataRole is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		for i in dataRole:
+			listeRoleName.append(i[0])
 
-	# DELETE tous les anciens roles de l'user
-	sql = "DELETE FROM account_role WHERE account_id = ?"
-	data = Database.request(sql, (dataAccountId[0][0],))
-	if data is None:
-		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 2")
-	
-	# INSERT tous les nouveaux roles de l'user
-	if len(listeRoleId) != 0:
-		for roleId in listeRoleId:
-			sql = "INSERT INTO account_role (account_id, role_id) VALUES (?, ?)"
-			Database.request(sql, (dataAccountId[0][0], roleId))
+		finalData = {
+			"uid": data[0][0],
+			"username": data[0][1],
+			"password": data[0][2],
+			"role": listeRoleName,
+			"createdAt": data[0][3],
+			"updatedAt": data[0][4]
+		}
 
-	# SELECT les infos de l'account après les modifications
-	sql = "SELECT uid, username, password, createdAt, updatedAt FROM account WHERE uid = ?"
-	data = Database.request(sql, (uid,))
-	if data is None:
-		return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error 3")
-	
-	finalData = {
-		"uid": data[0][0],
-		"username": data[0][1],
-		"password": data[0][2],
-		"role": listeRoleName,
-		"createdAt": data[0][3],
-		"updatedAt": data[0][4]
-	}
+		return returnAPIFormat(data=finalData, link=request.path, method=request.method)
 
-	return returnAPIFormat(data=finalData, link=request.path, method=request.method)
+	else:	# SI USER EST ADMIN
+
+		try:
+			roles = param["roles"]
+			if roles is None:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
+		except:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error")
+
+		if uid != "me":
+			# CHECK si l'account existe
+			sql = "SELECT id FROM account WHERE uid = ?"
+			dataAccountId = Database.request(sql, (uid,))
+			if dataAccountId is None:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+			if not dataAccountId:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=404)
+
+			bonId = dataAccountId[0][0]
+
+		else:	# SI uid = "me", pas besoin de select
+			bonId = dataUser[0][0]
+
+		# RECUP les uid des roles + CHECK s'ils existent
+		listeRoleId = []
+		listeRoleName = []
+		for i in roles:
+			sql = "SELECT id, name FROM role WHERE name = ?"
+			data = Database.request(sql, (i,))
+			if data is None:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+			if not data:
+				return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="Parameters Error, le role '"+i+"' n'existe pas")
+			listeRoleId.append(data[0][0])
+			listeRoleName.append(data[0][1])
+
+		# UPDATE de l'account
+		sql = "UPDATE account SET username = ?, password = ?, status = ?, updatedAt = current_timestamp WHERE id = ?"
+		data = Database.request(sql, (username, password, status, bonId))
+		if data is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+
+		# DELETE tous les anciens roles de l'user
+		sql = "DELETE FROM account_role WHERE account_id = ?"
+		data = Database.request(sql, (bonId,))
+		if data is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		
+		# INSERT tous les nouveaux roles de l'user
+		if len(listeRoleId) != 0:
+			for roleId in listeRoleId:
+				sql = "INSERT INTO account_role (account_id, role_id) VALUES (?, ?)"
+				Database.request(sql, (bonId, roleId))
+
+		# SELECT les infos de l'account après les modifications
+		sql = "SELECT uid, username, password, createdAt, updatedAt FROM account WHERE id = ?"
+		data = Database.request(sql, (bonId,))
+		if data is None:
+			return returnAPIFormat(data=None, link=request.path, method=request.method, status=422, message="SQL Error")
+		
+		finalData = {
+			"uid": data[0][0],
+			"username": data[0][1],
+			"password": data[0][2],
+			"role": listeRoleName,
+			"createdAt": data[0][3],
+			"updatedAt": data[0][4]
+		}
+
+		return returnAPIFormat(data=finalData, link=request.path, method=request.method)
+		
